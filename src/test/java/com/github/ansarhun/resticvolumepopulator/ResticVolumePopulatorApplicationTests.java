0a3616108a89b63dfef8f2ac46803dd94c8e9c54
Balazs Kocsi <ansar.hun@gmail.com>
2024-03-20 19:32:42 +0100

    src/test/.../ResticVolumePopulatorApplicationTests: hook in mock server

--- src/test/java/com/github/ansarhun/resticvolumepopulator/ResticVolumePopulatorApplicationTests.java
+++ src/test/java/com/github/ansarhun/resticvolumepopulator/ResticVolumePopulatorApplicationTests.java
@@ -1,12 +1,28 @@
 package com.github.ansarhun.resticvolumepopulator;
 
+import com.github.ansarhun.resticvolumepopulator.config.KubernetesConfiguration;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
-@EnableKubernetesMockClient
+@EnableKubernetesMockClient(crud = true)
 class ResticVolumePopulatorApplicationTests {
+
+	static KubernetesClient client;
+
+	@TestConfiguration
+	static class TestContextConfiguration {
+
+		@Bean
+		public KubernetesConfiguration.KubernetesClientBuilderCustomizer testKubernetesClient() {
+			return kubernetesClientBuilder ->
+					kubernetesClientBuilder.withConfig(client.getConfiguration());
+		}
+	}
 
 	@Test
 	void contextLoads() {
