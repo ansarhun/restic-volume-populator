998d8bbbd509433232b0062802b2305c043f7086
Balazs Kocsi <ansar.hun@gmail.com>
2025-08-20 09:09:52 +0200

    src/test/.../Versions: bump dependency versions
    Update K3S to v1.33.3 and Restic to v0.18.0.

--- src/test/java/com/github/ansarhun/resticvolumepopulator/Versions.java
+++ src/test/java/com/github/ansarhun/resticvolumepopulator/Versions.java
@@ -2,8 +2,8 @@ package com.github.ansarhun.resticvolumepopulator;
 
 public class Versions {
 
-    public static String K3S_VERSION = "rancher/k3s:v1.32.1-k3s1";
+    public static String K3S_VERSION = "rancher/k3s:v1.33.3-k3s1";
     public static String MINIO_VERSION = "minio/minio:RELEASE.2025-07-23T15-54-02Z";
-    public static String RESTIC_VERSION = "restic/restic:0.17.3";
+    public static String RESTIC_VERSION = "restic/restic:0.18.0";
 
 }
